b3a44667dd289caccde6fbf74f5a537f43416f94
vipin-mcsam <vipin@webinfomart.com>
2025-01-18 19:37:06 +0530

    Refactor SecretManagerService to AWSSecretsManagerService and update method names for clarity

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\SecretManagerService;
+use App\Services\AWSSecretsManagerService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -11,11 +11,10 @@ class ApiController extends Controller
     public function index(Request $request) {
 
         Log::info("Index function initialized");
-        $secretManager = new SecretManagerService();
-        $secrets = $secretManager->getSecret('MY_TEST_SECRET');
+        $secretManager = new AWSSecretsManagerService();
+        $secrets = $secretManager->getSecretValue('MY_TEST_SECRET');
         Log::info("SecretManager response:",[$secrets]);
         $envType = $secrets['USER_KEY'] ?? 'none';
-
         return [
             'status' => true,
             'key_type' => $envType,
--- app/Services/SecretManagerService.php
+++ app/Services/SecretManagerService.php
@@ -6,38 +6,35 @@ use Aws\SecretsManager\SecretsManagerClient;
 use Aws\Exception\AwsException;
 use Illuminate\Support\Facades\Log;
 
-class SecretManagerService
+class AWSSecretsManagerService
 {
     protected $client;
 
     public function __construct()
     {
         $this->client = new SecretsManagerClient([
-            'region' => env('AWS_DEFAULT_REGION'),
             'version' => 'latest',
-            'credentials' => [
-                'key'    => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            ],
+            'region'  => env('AWS_DEFAULT_REGION', 'eu-north-1'),
         ]);
     }
 
-    public function getSecret(string $secretName)
+    public function getSecretValue($secretName)
     {
         try {
-            $result = $this->client->getSecretValue(['SecretId' => $secretName]);
+            $result = $this->client->getSecretValue([
+                'SecretId' => $secretName,
+            ]);
 
             if (isset($result['SecretString'])) {
-                Log::info("Secret String retrieved...",[$result['SecretString']]);
-                return json_decode($result['SecretString'], true); // Decode the secret JSON
+                $secret = $result['SecretString'];
+                return json_decode($secret, true);
+            } else {
+                Log::error('SecretBinary is not supported in this example.');
+                return null;
             }
-            Log::info("Secret String not retrieved...",[$result]);
-            return null;
-
-
         } catch (AwsException $e) {
-            // Handle errors (log or throw)
-            Log::error("Error retrieving secret".$e->getMessage());
+            // Handle error
+            Log::error('Error retrieving secret: ' . $e->getMessage());
             return null;
         }
     }
